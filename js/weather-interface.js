ee3457437ac41b9238204e8380b238b4493dc29f
Eric Raetz and Margie Nevarez <eric@>
2017-01-04 11:16:25 -0800

    add complete weather object etc

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,13 +1,12 @@
-var apiKey = "4a7df8c5a3e8b29d61203cbd996ba2a8";
+var Weather = require('./../js/weather.js').weatherModule;
+
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();  
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
+  currentWeatherObject.getWeather(city);
+
   });
 });
