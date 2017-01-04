ff57db897a78b9af6711707ba703a746f0f4b7a2
Eric Raetz and Margie Nevarez <eric@>
2017-01-04 10:43:21 -0800

    initial files for gulp and bower, tested API weather

---
+++ js/weather-interface.js
@@ -0,0 +1,13 @@
+var apiKey = "4a7df8c5a3e8b29d61203cbd996ba2a8";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
+    });
+  });
+});
