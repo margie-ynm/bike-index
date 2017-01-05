7e60b813a7436f9670b2be31693dfe75dfb66c20
Eric Raetz and Margie Nevarez <eric@>
2017-01-05 11:39:09 -0800

    merge changes

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,13 +1,23 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
+
 $(document).ready(function() {
+  function toTitleCase(str)
+  {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
   var currentBikeObject = new Bike();
   $('#bikeLocation').click(function() {
     var city = $('#location').val();
     var radius = $('#radius').val();
+    var manufacturer = $('#manufacturer').val();
+    manufacturer = manufacturer.toLowerCase();
+    console.log(manufacturer);
+    $('#manufacturer').val("");
     $('#location').val("");
     $('#radius').val("");
     currentBikeObject.getStolenCount(city, radius);
     currentBikeObject.getStolenNames(city);
+    currentBikeObject.getAttributes(city, radius, manufacturer);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -1,10 +1,12 @@
 var apiKey = require('./../.env').apiKey;
-function Bike () {
-
+function Bike (city, radius, manufacturer) {
+  this.city = city;
+  this.radius = radius;
+  this.manufacturer = manufacturer;
 }
 
 Bike.prototype.getStolenCount = function(city, radius) {
-  $.get('https://bikeindex.org:443/api/v3/search/count?location=' + city + '&distance=' + radius + '&stolenness=stolen').then(function(response) {
+  $.get('https://bikeindex.org:443/api/v3/search/count?location=' + city + '&distance=' + radius + '&stolenness=proximity').then(function(response) {
     $('.showBikes').append("<h4>" + "The number of bicycles stolen in " + city + " within a radius of " + radius + " miles is " + response.proximity + ". " + "</h4>");
   }).fail(function(error) {
     $('.showBikes').text(error.responseJSON.message);
@@ -12,12 +14,30 @@ Bike.prototype.getStolenCount = function(city, radius) {
 };
 
 Bike.prototype.getStolenNames = function(city) {
-  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=' + city  + '&distance=10&stolenness=stolen').then(function(response) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=' + city  + '&distance=10&stolenness=proximity').then(function(response) {
     console.log(response);
     $('.showBikes').append("<p>" + "The types of bicycles stolen in " + city + " are the following: " + "</p>");
     response.bikes.forEach(function(bike) {
       $('.showBikes').append("<li>" + bike.title + "</li>");
     });
+  }).fail(function(error) {
+    $('.showBikes').text(error.responseJSON.message);
+  });
+};
+
+Bike.prototype.getAttributes = function(city, radius, manufacturer) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=' + city + '&distance=' + radius + '&stolenness=proximity').then(function(response) {
+    console.log(response);
+    var count = 0;
+    response.bikes.forEach(function(bike) {
+      var mn = bike.manufacturer_name;
+      mn = mn.toLowerCase();
+    if (mn === manufacturer) {
+      count++;
+      $('.middle-ground').append("<li>" + bike.title + "</li>");
+    }
+  });
+  $('.above-stuff').append("<p>" + "There are " + count + " bicycles that match your manufacturer:" + "</p>");
   }).fail(function(error) {
     $('.showBikes').text(error.responseJSON.message);
   });
